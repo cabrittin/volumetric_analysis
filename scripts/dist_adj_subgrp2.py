f451158e275b2a271ffd1ee9f66cc81d1b317bf3
cabrittin <cabrittin@gmail.com>
2018-11-07 01:00:19 -0500

    Added some documentation

--- scripts/dist_adj_subgrp2.py
+++ scripts/dist_adj_subgrp2.py
@@ -1,18 +1,28 @@
+"""
+dist_adj_subgrp2.py
+
+Plots adjacency degree distributions broken down by anatomical groups
+
+created: Christopher Brittin
+date: 01 November 2018 
+
+"""
+
+import sys
+sys.path.append(r'./volumetric_analysis')
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 #Brittin modules
-from Connectome.load import from_db
-from Networks.stats import *
-from Figures.stats import *
-import LUT
+from connectome.load import from_db
+from networks.stats import *
+from figures.stats import *
 import aux
 
 
 mpl.rcParams['xtick.labelsize'] = 32
 mpl.rcParams['ytick.labelsize'] = 32
 
-fout = '/home/cabrittin/Dropbox/PhD/sr_vol/figs2/dist_adj_subgrp2.png'
-neuron_class = '../Data/brittin_classv3.txt'
+neuron_class = './mat/nerve_ring_classes.txt'
 
 def get_group_index(A,grp):
     idx = []
@@ -23,10 +33,9 @@ def get_group_index(A,grp):
         idx = [v.index for v in A.vs.select(group=grp)]
     return idx
 
-def group_degrees(db):
+def group_degrees(db,_neuron_class):
     _remove = ['VC01','VD01','VB01','VB02']
-    loader = LUT.loader(db)
-    nclass = aux.read.into_dict(neuron_class)
+    nclass = aux.read.into_dict(_neuron_class)
 
     C = from_db(db,adjacency=True,remove=_remove)
     C.A.assign_membership_dict(nclass,key='group')
@@ -49,38 +58,19 @@ def group_degrees(db):
 
     return degrees
 
-n2u = group_degrees('N2U')
-jsh = group_degrees('JSH')
+def run(fout=None):
+    n2u = group_degrees('N2U',neuron_class)
+    jsh = group_degrees('JSH',neuron_class)
+    
+    data = []
+    for i in range(len(n2u)):
+        data.append(n2u[i])
+        data.append(jsh[i])
 
-data = []
-for i in range(len(n2u)):
-    data.append(n2u[i])
-    data.append(jsh[i])
+    fig,ax = plt.subplots(1,1,figsize=(12,10))
+    dist_adj_subgroups2(ax,data,fout=fout)
+    plt.show()
+        
 
-fig,ax = plt.subplots(1,1,figsize=(12,10))
-dist_adj_subgroups2(ax,data,fout=fout)
-plt.show()
-"""
-db = 'JSH'
-
-C = from_db(db,adjacency=True,remove=_remove)
-C.A.assign_membership_dict(nclass,key='group')
-
-s_idx = [v.index for v in C.A.vs.select(group='S')]
-i_idx = [v.index for v in C.A.vs.select(group='I')]
-m_idx = [v.index for v in C.A.vs.select(group='M')]
-
-data.append(C.A.degree(s_idx))
-data.append(C.A.degree(i_idx))
-data.append(C.A.degree(m_idx))
-
-
-
-fig,ax = plt.subplots(1,1,figsize=(12,10))
-dist_adj_subgroups(ax,data,fout=fout,)
-
-plt.tight_layout()
-plt.savefig(fout)
-plt.show()
-"""
-
+if __name__ == '__main__':
+    run()
