c1bfeec8638a3a345415a0b6f81a3476fd51f02d
Christopher Brittin <cabrittin@gmail.com>
2019-04-15 18:07:32 -0400

    simulated anneling added

--- cam_analysis/optimized_expression.py
+++ cam_analysis/optimized_expression.py
@@ -15,6 +15,8 @@ import argparse
 import numpy as np
 from random import sample
 from tqdm import tqdm
+from random import random
+import matplotlib.pyplot as plt
 
 from mat_loader import MatLoader
 from cam.expression import Matrix
@@ -50,27 +52,81 @@ def build_synapse_matices(cells,synapses):
     
     return P,A
 
-def run_optimization(E0,S,A,iters=None):
+
+def anneal(sol):
+    old_cost = cost(sol)
+    T = 1.0
+    T_min = 0.00001
+    alpha = 0.9
+    while T > T_min:
+        i = 1
+        while i <= 100:
+            new_sol = neighbor(sol)
+            new_cost = cost(new_sol)
+            ap = acceptance_probability(old_cost, new_cost, T)
+            if ap > random():
+                sol = new_sol
+                old_cost = new_cost
+            i += 1
+        T = T*alpha
+    return sol, cost
+
+def cost(E,S,A,gamma = [0.5,0.4,0.1]):
+    syn = np.sum(np.dot(E,S))
+    adj = np.sum(np.dot(E,A))
+    sparse = np.sum(E != 0)
+    J = gamma[0] * syn - gamma[1] * adj - gamma[2] * sparse
+    return J
+    
+def acceptance_probability(new_cost,old_cost,T):
+    p =  np.exp((new_cost - old_cost) / T)
+    return min(p,1)
+
+def perturb(coord,E):
+    rdx = np.random.randint(0,len(coord))
+    (a,b) = coord[rdx]
+    E[a,b] = int(not E[a,b])
+    return E
+
+def run_optimization(E0,S,A,iters=100):
+    (n,m) = E0.shape
     E = np.zeros(E0.shape)
     coord = np.nonzero(E0)
-    coord = list(zip(coord[0],cord[1]))
+    coord = list(zip(coord[0],coord[1]))
     k = int(len(coord) / 2)
     for (a,b) in sample(coord,k): E[a,b] = 1
     if not iters: iters = 10*len(coord)
     
-    for i in tqdm(range(iters),desc="Annealing"):
-        syn = np.sum(np.dot(E,S))
-        adj = np.sum(np.dot(E,A))
-        sparse = np.sum(E != 0)
-        rdx = np.random.randint(0,k)
-        (a,b) = coord[rdx]
-        E[a,b] = int(not E[a,b])
-         
+    old_cost = cost(E,S,A)
+    T = 1.0
+    T_min = 0.00001
+    alpha = 0.9
+    cost_rec = [old_cost]
+    while T > T_min:
+        i = 1
+        while i <= iters:
+            #Perturb E
+            rdx = np.random.randint(0,len(coord))
+            (a,b) = coord[rdx]
+            E[a,b] = int(not E[a,b])
+            #rdx = np.random.randint(0,n)
+            #old_row = E[rdx,:]
+            #nonzero = np.nonzero(old_row)
+            #E[rdx,nonzero] = 0
 
-    #print(E.shape,S.shape,A.shape,syn.shape,adj.shape)
-    #print(np.linalg.norm(syn,ord=2),np.linalg.norm(adj,ord=2))
-    #print(np.sum(syn),np.sum(adj))
-    #print(np.sum(E != 0))
+            #Compute new cost 
+            new_cost = cost(E,S,A)
+            #Decide to accept perturbation
+            ap = acceptance_probability(new_cost,old_cost,T)
+            if ap > random():
+                old_cost = new_cost
+                cost_rec.append(new_cost)
+            else:
+                E[a,b] = int(not(E[a,b]))
+                #E[rdx,nonzero] = 1
+            i += 1
+        T *= alpha
+    return E,cost_rec
 
 cam = 'mat/cam_isoforms.txt'
 
@@ -102,11 +158,22 @@ if __name__=='__main__':
     e.assign_expression()
     e.binarize()
     
-    cell = 'AIYL'
+    cell = 'AIBL'
     neigh = sorted(C.A.neighbors(cell))
     genes,E = e.reduced_expression(neigh) 
     
     P,A = build_synapse_matices(neigh,S[cell])
 
-    run_optimization(E,P,A)
+    Eopt,cost_rec = run_optimization(E,P,A)
 
+
+    print(np.sum(E),np.sum(Eopt))
+    print(np.sum(np.dot(E,P)),np.sum(np.dot(E,A)))
+    print(np.sum(np.dot(Eopt,P)),np.sum(np.dot(Eopt,A)))
+    idx = set(np.nonzero(Eopt)[0])
+    _genes = [genes[i] for i in idx]
+    print(len(genes),len(_genes))
+    print(_genes)
+    plt.figure()
+    plt.plot(cost_rec,'b-')
+    plt.show()
