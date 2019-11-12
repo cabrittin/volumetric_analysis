767adf293959d81ffd4dd21a6c00dba074cac479
Christopher Brittin <cabrittin@gmail.com>
2019-11-12 18:00:37 -0500

    script to change color of trakem2 fiel

---
+++ scripts/set_trackem2_color.sh
@@ -0,0 +1,2 @@
+python parsetrakem2/set_class_colors.py -t ~/data/trakem2_data/jsh/jsh_trakem2.xml -n ~/repo/bitbucket/volumetric2/data/topological_clusters/all_modified/no_cb_adj_l35_deg4_cl_modified.csv -c ~/repo/bitbucket/volumetric2/data/topological_clusters/all_modified/no_cb_adj_l35_deg4_cl_modified_trakem2.clr -o ~/data/trakem2_data/jsh/jsh_colors.xml
+
